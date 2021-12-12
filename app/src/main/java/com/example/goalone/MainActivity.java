3a77ceaf6bc1609fcefc3cf91ec044051c100ec7
Sasmitha Dasanayaka <sasmithadasanayaka96@gmail.com>
2021-12-12 19:29:38 +0530

    Bottom tab navigation is set

---
+++ app/src/main/java/com/example/goalone/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.goalone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new HomeFragment()).commit();
+        bottomNavigationView.setSelectedItemId(R.id.homeFragment);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.homeFragment:
+                        fragment = new HomeFragment();
+//                        bottomNavigationView.setSelectedItemId(R.id.homeFragment);
+                        break;
+                    case R.id.logsFragment:
+                        fragment = new LogsFragment();
+//                        bottomNavigationView.setSelectedItemId(R.id.logsFragment);
+                        break;
+                    case R.id.settingsFragment:
+                        fragment = new SettingsFragment();
+//                        bottomNavigationView.setSelectedItemId(R.id.settingsFragment);
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, fragment).commit();
+                return false;
+            }
+        });
+    }
+}
