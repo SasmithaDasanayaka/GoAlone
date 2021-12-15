937e1bf7394daf4027c2749c7070fff7b6b12f83
missakaherath <missakaherath96@gmail.com>
2021-12-15 11:01:28 +0530

    added recycler view

--- app/src/main/java/com/example/goalone/LogsFragment.java
+++ app/src/main/java/com/example/goalone/LogsFragment.java
@@ -3,40 +3,28 @@ package com.example.goalone;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LogsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LogsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView recyclerView;
+
+    private String[] warningMsg, time;
+    private int images[] = {R.drawable.danger, R.drawable.warning, R.drawable.caution, R.drawable.safer};
 
     public LogsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LogsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static LogsFragment newInstance(String param1, String param2) {
         LogsFragment fragment = new LogsFragment();
         Bundle args = new Bundle();
@@ -49,16 +37,22 @@ public class LogsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        warningMsg = getResources().getStringArray(R.array.warning_messages);
+        time = getResources().getStringArray(R.array.time);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_logs, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_logs, container, false);
+
+        recyclerView = v.findViewById(R.id.recyclerView);
+
+        MyAdapter myAdapter = new MyAdapter(this.getContext(), warningMsg, time, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        return v;
     }
 }
