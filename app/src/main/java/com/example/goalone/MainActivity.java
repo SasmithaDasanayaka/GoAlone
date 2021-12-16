fb8faeff97fe13c250ea1cdd8191be78b7c7a6ea
Sasmitha Dasanayaka <sasmithadasanayaka96@gmail.com>
2021-12-16 19:27:13 +0530

    Bluetooth enabled on application

--- app/src/main/java/com/example/goalone/MainActivity.java
+++ app/src/main/java/com/example/goalone/MainActivity.java
@@ -8,11 +8,15 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -22,7 +26,9 @@ import com.google.android.material.navigation.NavigationBarView;
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
-    private NavController navController;
+    private BluetoothAdapter bluetoothAdapter;
+    private ListView deviceListView;
+    private ArrayAdapter deviceArrayAdaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +39,24 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
         getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new HomeFragment()).commit();
         bottomNavigationView.setSelectedItemId(R.id.homeFragment);
+
+        deviceListView = (ListView) findViewById(R.id.deviceListView);
+//        deviceArrayAdaptor = new ArrayAdapter<String>(this, R.layout.fragment_home);
+//        deviceListView.setAdapter(deviceArrayAdaptor);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "BLUETOOTH NOT SUPPORTED", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment = null;
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                        switch (item.getItemId()) {
+                switch (item.getItemId()) {
                     case R.id.homeFragment:
                         fragment = new HomeFragment();
 //                        bottomNavigationView.setSelectedItemId(R.id.homeFragment);
@@ -52,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
 //                        bottomNavigationView.setSelectedItemId(R.id.settingsFragment);
                         break;
                 }
-                transaction.replace(R.id.nav_host_fragment, fragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, fragment).commit();
                 return true;
             }
         });
