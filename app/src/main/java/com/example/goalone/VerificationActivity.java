0c287c2d35b244953fddef77f3ed6dc072c04042
missakaherath <missakaherath96@gmail.com>
2021-12-20 14:24:05 +0530

    adding oauth

--- app/src/main/java/com/example/goalone/VerificationActivity.java
+++ app/src/main/java/com/example/goalone/VerificationActivity.java
@@ -7,6 +7,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -14,11 +16,19 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.goalone.databinding.ActivityVerificationBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class VerificationActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityVerificationBinding binding;
+
+    private FirebaseAuth mAuth; // variable for FirebaseAuth class
+    private EditText edtPhone, edtOTP;  // field for phone and OTP
+
+    private Button verifyOTPBtn, generateOTPBtn;    // buttons for generating OTP and verifying OTP
+
+    private String verificationId;   // string for storing verification ID
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,9 @@ public class VerificationActivity extends AppCompatActivity {
 
         binding = ActivityVerificationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // getting instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
 
         //setSupportActionBar(binding.toolbar);
 
